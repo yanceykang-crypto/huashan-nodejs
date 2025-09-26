33b32aba5db61437b4433cadef2f947e54408dcb
yangjianxin <150018405@qq.com>
2025-09-26 21:03:37 +0800

    up

--- .next/static/chunks/src_app_products_details_[id]_page_tsx_b2406eb4._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_96005ca2._.js",
-  "static/chunks/src_app_5c14addb._.js"
-],
-    source: "dynamic"
-});
